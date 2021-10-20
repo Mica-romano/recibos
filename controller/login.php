2bf4bc80d66e632cdde89984416e77634546e0c1
Mica-romano <mica.mdp95@gmail.com>
2021-10-20 17:12:58 -0300

    conexion de bd

---
+++ controller/login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+include('connectbd.php');
+//incluye el archivo de la conexion de la bd
+
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contrasenna'];
+
+$_SESSION['usuario']=$usuario;
+
+
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and contrasenna='$contraseña'";
+
+$resultado=mysqli_query($conexion,$consulta);
+$filas=mysqli_num_rows($resultado)
+
+if($filas){
+    header("location:users/index.html");
+
+}else{
+    ?>
+    <?php
+    include("index.php");
+    ?>
+    <h1>Datos invalidos</h1>
+    <?php
+}
+
+msqli_free_result($resultado);
+msqli_close($conexion);
+
+?>
